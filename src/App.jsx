4c5fadc329b40a79b227f1325982304de2695c37
OLEANG <wannachok.job@gmail.com>
2024-10-23 13:14:41 +0700

    Oleang-answer-form-Handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greeting, setGreeting] = useState("");
+  const [display, setDisplay] = useState("Greeting Message");
+
+  const handleGreeting = (event) => {
+    setGreeting(event.target.value);
+  }
+
+  const handleDisplay = () => {
+    setDisplay(greeting);
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{display}</div>
       <div className="input-container">
-        <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <label htmlFor="greeting-message">Greeting Message</label>
+        <input id="greeting-message" type="text" onChange={handleGreeting} />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleDisplay}>Update text</button>
       </div>
     </div>
   );
